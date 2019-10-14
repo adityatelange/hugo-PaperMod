ba297661ff44f2e60c96f0640a4c4d23760826df
nanxiaobei <nanxiaobei@gmail.com>
2019-10-15 01:31:07 +0800

    Add dark mode

--- static/main.js
+++ static/main.js
@@ -1,24 +1,17 @@
+// Highlight
 hljs.initHighlightingOnLoad();
 
-const listBg = document.querySelector('.list-bg');
-
-if (listBg) {
-  const halfAnHour = 1000 * 60 * 30;
-
-  function setListBgStyle() {
-    const now = new Date();
-    const time = now.getHours() + now.getMinutes() / 60;
-
-    let opacity = Math.abs(time - 14);
-    opacity = opacity > 12 ? 10 + Math.abs(opacity - 14) : opacity;
-    opacity = Number((opacity / 12).toFixed(2));
-
-    listBg.setAttribute('style', `opacity: ${opacity}`);
+// SunriseSunset
+const getSunrise = SunriseSunsetJS.getSunrise;
+const getSunset = SunriseSunsetJS.getSunset;
+navigator.geolocation.getCurrentPosition((position) => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const sunrise = getSunrise(latitude, longitude).getTime();
+  const sunset = getSunset(latitude, longitude).getTime();
+  const now = Date.now();
+  if (now < sunrise || now < sunset) {
+    const body = document.querySelector('body');
+    body.classList.add('dark');
   }
-
-  requestAnimationFrame(setListBgStyle);
-
-  setInterval(() => {
-    requestAnimationFrame(setListBgStyle);
-  }, halfAnHour);
-}
+});
