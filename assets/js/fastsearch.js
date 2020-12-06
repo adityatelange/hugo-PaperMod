454da9c1935b5780dc437581257fa58d794dc52c
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2020-12-06 18:10:44 +0530

    search: improve fastsearch

--- assets/js/fastsearch.js
+++ assets/js/fastsearch.js
@@ -1,61 +1,57 @@
 var fuse; // holds our search engine
 
+// load our search index, only executed onload
+function loadSearch() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                if (data) {
+                    // fuse.js options; check fuse.js website for details
+                    var options = {
+                        isCaseSensitive: false,
+                        shouldSort: true,
+                        location: 0,
+                        distance: 100,
+                        threshold: 0.4,
+                        minMatchCharLength: 0,
+                        keys: [
+                            'title',
+                            'permalink',
+                            'summary',
+                            'content'
+                        ]
+                    };
+                    fuse = new Fuse(data, options); // build the index from the json file
+                }
+            } else {
+                console.log(xhr.responseText);
+            }
+        }
+    };
+    xhr.open('GET', "../index.json");
+    xhr.send();
+}
+
 // execute search as each character is typed
 document.getElementById("searchInput").onkeyup = function (e) {
     // run a search query (for "term") every time a letter is typed
     // in the search box
-    let results = fuse.search(this.value); // the actual query being run using fuse.js
-    let searchitems = ''; // our results bucket
+    const results = fuse.search(this.value); // the actual query being run using fuse.js
 
-    if (results.length === 0) { // no results based on what was typed into the input box
-        searchitems = '';
-    } else { // build our html
-        for (let item in results) { // only show first 5 results
-            searchitems = searchitems +
-                `<li class="post-entry">
-                <header class="entry-header">${results[item].item.title}&nbsp;»</header>
-                <a href="${results[item].item.permalink}"></a>
-                </li>`;
+    if (results.length !== 0) {
+        // build our html if result exists
+        let resultSet = ''; // our results bucket
+
+        for (let item in results) {
+            resultSet = resultSet + itemGen(results[item].item.title, results[item].item.permalink)
         }
+
+        document.getElementById("searchResults").innerHTML = resultSet;
     }
-
-    document.getElementById("searchResults").innerHTML = searchitems;
 }
 
-
-// fetch some json
-function fetchJSONFile(path, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function () {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
-            }
-        }
-    };
-    httpRequest.open('GET', path);
-    httpRequest.send();
-}
-
-// load our search index, only executed once
-function loadSearch() {
-    fetchJSONFile('../index.json', function (data) {
-        // fuse.js options; check fuse.js website for details
-        var options = {
-            isCaseSensitive: false,
-            shouldSort: true,
-            location: 0,
-            distance: 100,
-            threshold: 0.4,
-            minMatchCharLength: 0,
-            keys: [
-                'title',
-                'permalink',
-                'summary',
-                'content'
-            ]
-        };
-        fuse = new Fuse(data, options); // build the index from the json file
-    });
+function itemGen(name, link) {
+    return `<li class="post-entry"><header class="entry-header">${name}&nbsp;»</header><a href="${link}" aria-label="${name}"></a></li>`
 }
