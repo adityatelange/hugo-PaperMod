a4b20f0f4d4aaa05154c0dd6b94f0c88238487d8
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2022-05-29 11:11:26 +0530

    Add license text for PaperMod in resources

---
+++ assets/js/license.js
@@ -0,0 +1,6 @@
+/*
+  PaperMod v6
+  License: MIT https://github.com/adityatelange/hugo-PaperMod/blob/master/LICENSE
+  Copyright (c) 2020 nanxiaobei and adityatelange
+  Copyright (c) 2021-2022 adityatelange
+*/
