56560e3a0333dc5e3f1986c79f6c870b18fcb0d2
Wonderfall <wonderfall@protonmail.com>
2022-07-29 01:59:55 +0200

    make the progress bar disappear near the bottom

--- assets/js/papermod.js
+++ assets/js/papermod.js
@@ -116,8 +116,9 @@ function progressBar() {
     var bar = document.getElementById("progressBar");
 
     document.addEventListener('scroll', () => {
-        var scrollPercent = document.documentElement.scrollTop / (document.documentElement.scrollHeight - window.innerHeight) * 100 + '%';
-        bar.style.setProperty("--scrollAmount", scrollPercent);
+        var scrollPercent = document.documentElement.scrollTop / (document.documentElement.scrollHeight - window.innerHeight) * 100;
+        if (scrollPercent >= 99) { scrollPercent = 0 };
+        bar.style.setProperty("--scrollAmount", scrollPercent + '%');
     })
 }
 
