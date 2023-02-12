25e5b4d2982aacfee9a1194d2738db284c2ac380
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2023-02-12 12:23:08 +0530

    Tag Release 7.0

--- assets/css/core/license.css
+++ assets/css/core/license.css
@@ -1,5 +1,5 @@
 /*
-  PaperMod v6
+  PaperMod v7
   License: MIT https://github.com/adityatelange/hugo-PaperMod/blob/master/LICENSE
   Copyright (c) 2020 nanxiaobei and adityatelange
   Copyright (c) 2021-2023 adityatelange
