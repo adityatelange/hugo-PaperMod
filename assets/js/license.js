040aa83ae60f493c2db8cbf945bd6d5a77cf821d
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2024-11-09 21:36:05 +0530

    Update PaperMod version to v8+ in license.css and license.js

--- assets/js/license.js
+++ assets/js/license.js
@@ -1,5 +1,5 @@
 /*
-  PaperMod v7
+  PaperMod v8+
   License: MIT https://github.com/adityatelange/hugo-PaperMod/blob/master/LICENSE
   Copyright (c) 2020 nanxiaobei and adityatelange
   Copyright (c) 2021-2024 adityatelange
